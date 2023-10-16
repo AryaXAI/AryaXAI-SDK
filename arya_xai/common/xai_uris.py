504eb148ca7f8d3f8ade2e6ed6db0308f2b2edb6
Amey Balekundri <amey.balekundri@arya.ai>
2023-10-16 12:50:53 +0530

    add documentaion for workspaces

--- arya_xai/common/xai_uris.py
+++ arya_xai/common/xai_uris.py
@@ -14,7 +14,7 @@ UPDATE_WORKSPACE_URI = f'{API_VERSION}/users/workspace_config_update'
 CREATE_PROJECT_URI = f'{API_VERSION}/users/create_project'
 
 # Project
-UPLOAD_DATA_FILE = f'{API_VERSION}/users/uploadfile'
-GET_DATA_SUMMARY = f'{API_VERSION}/project/data_summary'
+UPLOAD_DATA_FILE_URI = f'{API_VERSION}/users/uploadfile'
+GET_DATA_SUMMARY_URI = f'{API_VERSION}/project/data_summary'
 GET_DATA_DIAGNOSIS_URI = f'{API_VERSION}/project/get_data_diagnosis'
 GET_DATA_DRIFT_DIAGNOSIS_REPORT_URI = f'{API_VERSION}/project/get_data_diagnosis_drift_report'
