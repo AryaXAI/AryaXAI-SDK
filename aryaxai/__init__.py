2c2fd6b858b65aee417f6dd9aad502197c2fa319
Rahul <rahul.chodvadiya@gmail.com>
2023-11-01 00:13:43 +0530

    auto release to test pypi/pypi based on pushed changes

--- aryaxai/__init__.py
+++ aryaxai/__init__.py
@@ -1,10 +1,8 @@
 import os
 
-env = os.environ.get('XAI_ENV', 'prod')
-logger_on = os.environ.get('DEBUG', False)
+env = os.environ.get('XAI_ENV', 'testing')
 
-if logger_on:
-    print(f'Welcome, you are using AryaXAI in {env} environment.')
+print(f'Welcome, you are using AryaXAI in {env} environment.')
 
 #import core modules so that they can be imported directly
 from aryaxai.core.xai import XAI
