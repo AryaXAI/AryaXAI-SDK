7693340b2a28d8046741048ff4741cdd99371a8c
Rahul <rahul.chodvadiya@gmail.com>
2023-11-01 00:19:05 +0530

    remove log on importing package

--- aryaxai/__init__.py
+++ aryaxai/__init__.py
@@ -1,9 +1,3 @@
-import os
-
-env = os.environ.get('XAI_ENV', 'testing')
-
-print(f'Welcome, you are using AryaXAI in {env} environment.')
-
 #import core modules so that they can be imported directly
 from aryaxai.core.xai import XAI
 
