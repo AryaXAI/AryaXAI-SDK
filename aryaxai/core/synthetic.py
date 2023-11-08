4b4988408677c63be8c8f1fd40523d0d29602bee
Rahul <rahul.chodvadiya@gmail.com>
2023-11-08 11:02:08 +0530

    add method to plot psi chart for synthetic tag data

--- aryaxai/core/synthetic.py
+++ aryaxai/core/synthetic.py
@@ -2,6 +2,9 @@ from typing import List
 from pydantic import BaseModel
 import pandas as pd
 
+import plotly.graph_objects as go
+
+
 class SyntheticDataTag(BaseModel):
     info: dict
     metadata: dict
@@ -12,7 +15,35 @@ class SyntheticDataTag(BaseModel):
 
     def plot_psi(self):
         """plot psi chart"""
-        pass
+        x_data = [item["Column"] for item in self.plot_data]
+        y_data = [item["Quality Score"] for item in self.plot_data]
+        metric_data = [item["Metric"] for item in self.plot_data]
+
+        traces = []
+        for metric in set(metric_data):
+            indices = [i for i, val in enumerate(metric_data) if val == metric]
+            traces.append(
+                go.Bar(
+                    x=[x_data[i] for i in indices],
+                    y=[y_data[i] for i in indices],
+                    name=metric
+                )
+            )
+
+        fig = go.Figure(data=traces)
+
+        fig.update_layout(
+            barmode="relative",
+            xaxis_title="Column",
+            yaxis_title="Quality Score",
+            height=500,
+            bargap=0.01,
+            legend_orientation="h",
+            legend_x=0.1,
+            legend_y=1.1,
+        )
+
+        fig.show(config={"displaylogo": False})
 
     def view_info(self) -> pd.DataFrame:
         """pretty print the synthetic data tag info
@@ -25,13 +56,13 @@ class SyntheticDataTag(BaseModel):
 
     def view_metadata(self) -> pd.DataFrame:
         """pretty print the synthetic data metadata
-        
+
         :return: DataFrame
         """
         metadata = {k: v for k, v in self.metadata.items() if v is not None}
 
         return pd.DataFrame(metadata, index=[0])
-        
+
     def __print__(self) -> str:
         return f"SyntheticDataTag({self.info})"
 
@@ -40,7 +71,7 @@ class SyntheticDataTag(BaseModel):
 
     def __repr__(self) -> str:
         return self.__print__()
-    
-    
+
+
 class SyntheticModel:
     pass
